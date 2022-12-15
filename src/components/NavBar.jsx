39612a420a002d8aaa9e54fad8aec42c10fa818f
Guillaume Sere <guillaumesere@hotmail.fr>
2022-12-15 11:06:57 +0100

    add toggle theme dark

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../images/logo.png'
+import DayNightToggle from 'react-day-and-night-toggle'
 
 const NavBar = () => {
-  return (
-    <div className='navbar'>
-        <Link className='nav-link' to="/">ACCUEIL</Link>
-        <Link className='nav-link' to="/curiosity">CURIOSITY</Link>
-        <Link className='nav-link' to="/asteroide">ASTEROÏDES</Link>
-        <Link className='nav-link' to="/epic">PHOTOS NASA</Link>
-        <img className='logo' src={logo} alt="logo" />
-    </div>
-  )
+
+    const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('data-theme') === 'dark' ? true : false)
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+        const newColorScheme = e.matches ? 'dark' : 'light'
+
+        setIsDarkMode(newColorScheme === 'dark' ? true : false)
+        localStorage.setItem('data-theme', newColorScheme)
+        document.body.setAttribute('data-theme', localStorage.getItem('data-theme'))
+    })
+
+    const handleChangeTheme = () => {
+        setIsDarkMode(!isDarkMode)
+        if (!isDarkMode) {
+            localStorage.setItem('data-theme', 'dark')
+            document.body.setAttribute('data-theme', 'dark')
+        } else {
+            localStorage.setItem('data-theme', 'light')
+            document.body.setAttribute('data-theme', 'light')
+        }
+    }
+
+    return (
+        <div className='navbar'>
+            <Link className='nav-link' to="/">ACCUEIL</Link>
+            <Link className='nav-link' to="/curiosity">CURIOSITY</Link>
+            <Link className='nav-link' to="/asteroide">ASTEROÏDES</Link>
+            <Link className='nav-link' to="/epic">PHOTOS NASA</Link>
+            <DayNightToggle
+                onChange={handleChangeTheme}
+                checked={isDarkMode}
+                className="toggle-theme"
+            />
+            <img className='logo' src={logo} alt="logo" />
+        </div>
+    )
 }
 
 export default NavBar
