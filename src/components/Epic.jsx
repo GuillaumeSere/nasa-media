5710db0620bc0beceb2d0952ddffd824e079dd0b
Guillaume Sere <guillaumesere@hotmail.fr>
2023-12-08 13:57:48 +0100

    add pagination

--- src/components/Epic.jsx
+++ src/components/Epic.jsx
@@ -1,46 +1,78 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
-import animation from '../images/107321-rocket.json'
+import animation from '../images/107321-rocket.json';
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
 
 const Epic = () => {
+  const [epic, setEpic] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const photosPerPage = 6;
 
-    const [epic, setEpic] = useState(null)
+  useEffect(() => {
+    fetchPlanete();
+    async function fetchPlanete() {
+      const res = await fetch(`https://images-api.nasa.gov/search?q=apollo%2022`);
+      const data = await res.json();
+      setEpic(data);
+    }
+  }, [currentPage]);
 
-    useEffect(() => {
-        fetchPlanete()
-        async function fetchPlanete() {
-            const res = await fetch(
-                `https://images-api.nasa.gov/search?q=apollo%2022`
-            )
-            const data = await res.json()
-            setEpic(data)
-        }
-    }, [])
+  const nextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
 
-    if (!epic) return <div />
+  const prevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
 
-    return (
-        <>
-        <Player
+  const openImageInNewWindow = (imageUrl) => {
+    window.open(imageUrl, '_blank');
+  };
+
+  if (!epic) return <div />;
+
+  const totalPhotos = epic.collection.items.length;
+  const totalPages = Math.ceil(totalPhotos / photosPerPage);
+
+  return (
+    <>
+      <Player
         autoplay
         loop
         src={animation}
         style={{ height: '300px', width: '300px', paddingTop: '5rem', marginBottom: '-12rem', marginLeft: '10rem' }}
-    >
-    </Player>
-        <div className='container-post'>
-            {epic.collection.items.map((post, index) => {
-                return (
-                    <div className='epic-card' key={index}>
-                        <div className='epic-title'>{post.data[0].title}</div>
-                        <img className='epic-image' src={post.links[0].href} alt="" />
-                        <div className='epic-description'>{post.data[0].description}</div>
-                    </div>
-                )
-            })}
-        </div>
-        </>
-    )
-}
+      ></Player>
+      <div className='container-post'>
+        {epic.collection.items.slice((currentPage - 1) * photosPerPage, currentPage * photosPerPage).map((post, index) => {
+          return (
+            <div className='epic-card' key={index}>
+              <div className='epic-title'>{post.data[0].title}</div>
+              <img
+                className='epic-image'
+                src={post.links[0].href}
+                alt=''
+                onClick={() => openImageInNewWindow(post.links[0].href)}
+              />
+              <div className='epic-description'>{post.data[0].description}</div>
+            </div>
+          );
+        })}
+      </div>
+      <div className='pagination'>
+        <button onClick={prevPage} disabled={currentPage === 1}>
+          <FaChevronLeft />
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button key={index + 1} onClick={() => setCurrentPage(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+        <button onClick={nextPage} disabled={currentPage === totalPages}>
+          <FaChevronRight />
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default Epic
+export default Epic;
--- src/components/NasaPhoto.jsx
+++ src/components/NasaPhoto.jsx
@@ -1,48 +1,84 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
-import animation from '../images/28514-mars-2020-nasa-mission.json'
+import animation from '../images/28514-mars-2020-nasa-mission.json';
+import { FaChevronRight } from "react-icons/fa";
+import { FaChevronLeft } from "react-icons/fa";
 
 const NasaPhoto = () => {
+  const [photoData, setPhotoData] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const photosPerPage = 6;
 
-    const [photoData, setPhotoData] = useState(null)
+  useEffect(() => {
+    fetchPhoto();
+    async function fetchPhoto() {
+      const res = await fetch(
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=${currentPage}&api_key=${process.env.REACT_APP_API_KEY}`
+      );
+      const data = await res.json();
+      setPhotoData(data);
+    }
+  }, [currentPage]);
 
-    useEffect(() => {
-        fetchPhoto()
-        async function fetchPhoto() {
-            const res = await fetch(
-                `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.REACT_APP_API_KEY}`
-            )
-            const data = await res.json()
-            setPhotoData(data)
-        }
-    }, [])
+  const nextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
 
-    if (!photoData) return <div />
+  const prevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
 
-    return (
-        <>
-        <Player
+  const openImageInNewWindow = (imageUrl) => {
+    window.open(imageUrl, '_blank');
+  };
+
+  if (!photoData) return <div />;
+
+  const totalPhotos = photoData.photos.length;
+  const totalPages = Math.ceil(totalPhotos / photosPerPage);
+
+  return (
+    <>
+      <Player
         autoplay
         loop
         src={animation}
         className='curiosity'
         style={{ height: '400px', width: '400px', paddingTop: '5rem', marginBottom: '-14rem' }}
-    >
-    </Player>
-        <div className='container-post'>
-            {photoData.photos.map((post, index) => {
-                return (
-                    <div className='post-card' key={index}>
-                        <span className='post-date'>Date: {post.earth_date}</span>
-                        <div className='post-name'>Camera: {post.camera.full_name}</div>
-                        <div className='post-rover'>Robot: {post.rover.name}</div>
-                        <img className='post-image' src={post.img_src} alt="illustration" />
-                    </div>
-                )
-            })}
-        </div>
-        </>
-    )
-}
+      ></Player>
+      <div className='container-post'>
+        {photoData.photos.slice((currentPage - 1) * photosPerPage, currentPage * photosPerPage).map((post, index) => {
+          return (
+            <div className='post-card' key={index}>
+              <span className='post-date'>Date: {post.earth_date}</span>
+              <div className='post-name'>Camera: {post.camera.full_name}</div>
+              <div className='post-rover'>Robot: {post.rover.name}</div>
+              <img
+                className='post-image'
+                src={post.img_src}
+                alt='illustration'
+                onClick={() => openImageInNewWindow(post.img_src)}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className='pagination'>
+        <button onClick={prevPage} disabled={currentPage === 1}>
+        <FaChevronLeft />
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button key={index + 1} onClick={() => setCurrentPage(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+        <button onClick={nextPage} disabled={currentPage === totalPages}>
+        <FaChevronRight />
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default NasaPhoto
+export default NasaPhoto;
+
