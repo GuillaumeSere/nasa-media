76c45b2e5afe9c992aff5d48a7c8071d95ee1093
Guillaume Sere <guillaumesere@hotmail.fr>
2024-11-28 13:20:02 +0100

    add menu burger

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,6 +8,7 @@ import animation from '../images/23661-nasa-logo.json'
 const NavBar = () => {
 
     const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('data-theme') === 'dark' ? true : false)
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
         const newColorScheme = e.matches ? 'dark' : 'light'
@@ -28,28 +29,38 @@ const NavBar = () => {
         }
     }
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
     return (
         <div className='navbar'>
-            <Link className='nav-link' to="/">ACCUEIL</Link>
-            <Link className='nav-link' to="/curiosity">CURIOSITY</Link>
-            <Link className='nav-link' to="/asteroide">ASTEROÏDES</Link>
-            <Link className='nav-link' to="/epic">PHOTOS NASA</Link>
-            <Link className='nav-link' to="/recherche">RECHERCHE</Link>
-            <DayNightToggle
-                onChange={handleChangeTheme}
-                checked={isDarkMode}
-                className="toggle-theme"
-                size= "20"
-            />
-            {/* { <img className='logo' src={logo} alt="logo" />} */}
-            <Player
-                autoplay
-                loop
-                src={animation}
-                className='logo'
-                style={{ height: '80px', width: '100px', marginTop: '-2.5rem'}}
-            >
-            </Player>
+            <div className='toggle-menu' onClick={toggleMenu}>
+                <span className={`hamburger-line ${isMenuOpen ? 'active' : ''}`}></span>
+                <span className={`hamburger-line ${isMenuOpen ? 'active' : ''}`}></span>
+                <span className={`hamburger-line ${isMenuOpen ? 'active' : ''}`}></span>
+                <Player
+                    autoplay
+                    loop
+                    src={animation}
+                    className='logo'
+                    style={{ height: '80px', width: '100px', marginTop: '-2.5rem', zIndex:'1000'}}
+                >
+                </Player>
+            </div>
+            <div className={`menu ${isMenuOpen ? 'active' : ''}`}>
+                <Link className='nav-link' to="/">ACCUEIL</Link>
+                <Link className='nav-link' to="/curiosity">CURIOSITY</Link>
+                <Link className='nav-link' to="/asteroide">ASTEROÏDES</Link>
+                <Link className='nav-link' to="/epic">PHOTOS NASA</Link>
+                <Link className='nav-link' to="/recherche">RECHERCHE</Link>
+                <DayNightToggle
+                    onChange={handleChangeTheme}
+                    checked={isDarkMode}
+                    className="toggle-theme"
+                    size= "15"
+                />
+            </div>
         </div>
     )
 }
